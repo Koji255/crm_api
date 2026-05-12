b5a7fd7ece4ff0be4f7f16f760dec1a3a12b337d
Koji255 <koji25ony1@gmail.com>
2026-05-12 23:23:04 +0300

    compose: backend, db, redis, celery-worker, celery-beat

--- djangoproject/backend/settings.py
+++ djangoproject/backend/settings.py
@@ -152,10 +152,10 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': crontab(hour=0, minute=0),
     },
 }
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_TIMEZONE = 'Europe/Moscow'
 # disable broker (for tests)
 # CELERY_TASK_ALWAYS_EAGER = True
